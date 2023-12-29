9072bce4f9d3dc9a2fa361bbd63d48e031b8efc3
Emanuel Pereira Indra <EX142664@edp.pt>
2023-12-29 12:00:29 +0000

    Add Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./App.css";
+import Atividade from "./components/Atividade";
+import AtividadeForm from "./components/AtividadeForm";
 
 
 let initialState = [
@@ -34,7 +36,7 @@ function App() {
 
   function apagarAtividade(id){
     const atividadesFiltradas = atividades.filter(atividades => atividades.id !== id );
-    setAtividades([... atividadesFiltradas])
+    setAtividades([...atividadesFiltradas])
   }
 
   function prioridadeLabel(param){
@@ -66,44 +68,10 @@ function App() {
 
   return (
     <>
-      <form className="row g-3">
-        <div className="col-md-6">
-          <label className="form-label">
-            Id
-          </label>
-          <input id="id" type="text" className="form-control" readOnly
-          value={Math.max.apply(Math, atividades.map(item => item.id)) + 1} />
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">
-            Prioridade
-          </label>
-          <select id="prioridade" className="form-select">
-            <option defaultValue="0" >Seleccione...</option>
-            <option value="1">Baixa</option>
-            <option value="2">Normal</option>
-            <option value="3">Alta</option>
-          </select>
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">
-            Título
-          </label>
-          <input id="titulo" type="text" className="form-control" />
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">
-            Descrição
-          </label>
-          <input id="descricao" type="text" className="form-control" />
-        </div>
-        <hr />
-        <div className="col-12">
-          <button className="btn btn-outline-secondary" onClick={addAtividade}>
-            + Atividade
-          </button>
-        </div>
-      </form>
+      <AtividadeForm 
+      addAtividade ={addAtividade}
+      atividades={atividades}
+       />
       <div className="mt-3">
         {atividades.map((ativ) => (
           <div key={ativ.id} className={"card mb-2 shadow-sm border-" + prioridadeStyle(ativ.prioridade)}>
@@ -138,7 +106,7 @@ function App() {
           </div>
         ))}
       </div>
-    </>
+      </>
   ); 
 }
 
